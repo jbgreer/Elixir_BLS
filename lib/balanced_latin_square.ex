d61947151bd31efcc006cf87291957739eb01aec
Jim Greer <jbgreer@gmail.com>
2023-09-03 14:05:01 -0500

    initial

---
+++ lib/balanced_latin_square.ex
@@ -0,0 +1,18 @@
+defmodule BalancedLatinSquare do
+  @moduledoc """
+  Documentation for `BalancedLatinSquare`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BalancedLatinSquare.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/balanced_latin_square_test.exs
@@ -0,0 +1,8 @@
+defmodule BalancedLatinSquareTest do
+  use ExUnit.Case
+  doctest BalancedLatinSquare
+
+  test "greets the world" do
+    assert BalancedLatinSquare.hello() == :world
+  end
+end
