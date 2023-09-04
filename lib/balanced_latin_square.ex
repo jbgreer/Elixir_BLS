5f67e05c55cd5a17dd455f13d2e5248c32c56ef5
Jim Greer <jbgreer@gmail.com>
2023-09-04 14:03:35 -0500

    doc focumatting, when guards, exception on negative argument

--- lib/balanced_latin_square.ex
+++ lib/balanced_latin_square.ex
@@ -2,14 +2,14 @@ defmodule BalancedLatinSquare do
   @moduledoc """
   Given a positive integer n, generate a Balanced Latin Square for n conditions
 
-  Take a single positive integer argument, the number of conditions (n)
-  When n is even
-     make the first row using the formula row1 = 1,2,n,3,n-1,n-2
-     fill in the first column sequentially (mod n)
-     continue filling in the columns sequentially until the square is completed
-  When n is odd
-     follow the above procedure for n even
-     append a second set of rows with the reversed contents of every row
+  Take a single positive integer argument, the number of conditions (n) .
+  - When n is even
+     - make the first row using the formula row = 1,2,n,3,n-1,n-2,...
+     - fill in the first column sequentially (mod n)
+     - continue filling in the columns sequentially until the square is completed
+  - When n is odd
+     - follow the procedure for n even
+     - append a second set of rows with the reversed contents of the even set's rows
 
   ## Exports
   `bls/1`: pos_integer()
@@ -43,8 +43,7 @@ defmodule BalancedLatinSquare do
       [[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
   """
   @spec bls(pos_integer()) :: nonempty_list(nonempty_list(pos_integer()))
-  # @spec bls(pos_integer()) :: List[List[pos_integer()]]
-  def bls(n) do
+  def bls(n) when is_integer(n) and n > 0 do
     square =
       for row <- 0..(n - 1) do
         for col <- 0..(n - 1) do
@@ -57,5 +56,9 @@ defmodule BalancedLatinSquare do
     else
       Enum.sort(square)
     end
+  end
+
+  def bls(n) when n < 1 do
+    raise ArgumentError, "argument must be a positive integer"
   end
 end
